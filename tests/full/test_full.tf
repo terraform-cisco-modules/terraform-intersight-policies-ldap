9d2a1e8d4ecc276aaf371f851d2d449b64b3c768
scotttyso <tyscott@cisco.com>
2022-10-07 13:40:37 -0400

    adding output to test_full

--- tests/full/test_full.tf
+++ tests/full/test_full.tf
@@ -23,15 +23,15 @@ variable "name" {}
 
 output "ldap_policy" {
   value = {
-    ldap_groups = length(var.ldap_groups) > 0 ? {
-      for v in sort(keys(intersight_iam_ldap_group.ldap_group)
-      ) : v => intersight_iam_ldap_group.ldap_group[v].moid
-    } : {}
-    ldap_providers = length(var.ldap_providers) > 0 ? {
-      for v in sort(keys(intersight_iam_ldap_provider.ldap_providers)
-      ) : v => intersight_iam_ldap_provider.ldap_providers[v].moid
-    } : {}
-    moid = intersight_iam_ldap_policy.ldap.moid
+    ldap_groups = {
+      for v in sort(keys(module.main.ldap_policy.ldap_groups)
+      ) : v => module.main.ldap_policy.ldap_groups[v]
+    }
+    ldap_providers = {
+      for v in sort(keys(module.main.ldap_policy.ldap_providers)
+      ) : v => module.main.ldap_policy.ldap_providers[v]
+    }
+    moid = module.main.moid
     name = var.name
   }
 }
